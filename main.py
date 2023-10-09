a0150567b346ea34fd98392b115a31c573d4844e
Diana Kuanyshkyzy <141010600+dianakuanyshkyzy@users.noreply.github.com>
2023-10-10 02:03:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -41,19 +41,9 @@ DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# SQLAlchemy model
 Base = declarative_base()
 
-# class Post(Base):
-#     __tablename__ = "posts"
-#     id = Column(Integer, primary_key=True, index=True)
-#     author = Column(String)
-#     text = Column(String)
-#     keywords = Column(String)
-#     created_at = Column(DateTime, default=datetime.now)
 
-# Create the table if not exists
-#Base.metadata.create_all(bind=engine)
 
 @app.on_event("startup")
 async def startup():
@@ -71,7 +61,7 @@ def generate_ad(keywords:str):
     headers = {
         'Authorization': "Bearer " + token
     }
-    response = requests.post('https://7583-185-48-148-173.ngrok-free.app/custom-prompt', headers=headers, json={
+    response = requests.post('Your_API', headers=headers, json={ #put your API here
         "input_text": f"generate a gift card in 100 words using these keywords: {keywords}"
     })
     body = response.json()
